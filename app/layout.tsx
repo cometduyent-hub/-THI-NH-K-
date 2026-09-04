7f98d0f16bb72d91dd76f81fae0130b54318dee2
cometduyent-hub <comet.duyent@gmail.com>
2026-09-04 14:19:53 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-import "./globals.css";
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "CHINH PHỤC KHTN CÙNG THẦY TUẤN",
-  description: "Hệ thống kiểm tra online KHTN"
+  title: "Physics Test Arena",
+  description: "Hệ thống kiểm tra trực tuyến môn Vật lí",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
- return (
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
     <html lang="vi">
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css" />
-        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.js"></script>
-        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/contrib/auto-render.min.js" onload="renderMathInElement(document.body);"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.8/katex.min.css" />
       </head>
       <body>{children}</body>
     </html>
